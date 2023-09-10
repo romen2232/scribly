c83c196be2ec3035b2b5fd6ced997a227aa09fff
romen2232 <lykaios2232@gmail.com>
2023-09-11 01:52:26 +0200

    fix(deploy): updated railway.json with front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/static/',
+    base: process.env.NODE_ENV === 'production' ? '/static/' : '/',
 });
