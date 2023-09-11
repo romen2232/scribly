a5ef6b0724dd95cd5ffabb04d4a3d856844aab5c
Romen Medina Beltrán <romenmedbel@gmail.com>
2023-09-11 10:29:34 +0200

    Update settings.py

--- backend/scribly_api/settings.py
+++ backend/scribly_api/settings.py
@@ -21,7 +21,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
-#load_dotenv(os.path.join(BASE_DIR, ".env"))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
